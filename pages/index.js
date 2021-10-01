87568a1c9f69f9cd7d7b8e59b0a28ec5f1909068
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-10-01 14:32:54 +0200

    firebase start

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import firebase from "../firebase/Firebase"
 
 export default function Home() {
   return (
